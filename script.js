eadbec2fd7dc8c7b24551bcc55a0364bc099054f
Shubham Upadhyay <shubhamup47@gmail.com>
2023-05-12 17:40:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 function threeSum(arr, target) {
-// write your code here
-  
+
+	let sum = 0;
+	
+	for(let i=0; i<arr.length; i++)
+		{
+			
+			for(let j=i+1; j<=3; j++)
+				{
+					if(arr[i] + arr[j] <= target)
+					{
+						sum = arr[i] + arr[j];
+					}
+				}
+		}
+  return sum;
 }
 
 module.exports = threeSum;
